c4606d160c8baccb4989e3b2ff531ef18414aee5
MrQuackDuck <mrquackduckk@gmail.com>
2022-11-23 22:27:45 +0200

    Small update

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,6 +1,6 @@
 let bodyHeight = document.querySelector("html").offsetHeight
 
-if (bodyHeight < 800) {
+if (bodyHeight < 900) {
     footer = document.querySelector("footer")
     footer.style.position = "fixed"
     footer.style.bottom = "0"
